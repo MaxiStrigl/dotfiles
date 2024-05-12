9afbe3cae8fbf76639ff6c9dc119a402cdc9a4a1
Maxi <max.03.strigl@hotmail.com>
2024-05-12 19:42:36 +0200

    few changes

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -1,28 +1,62 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-    "nvim-neotest/nvim-nio",
-		"leoluz/nvim-dap-go",
-		"rcarriga/nvim-dap-ui",
+	{
+		"mfussenegger/nvim-dap",
+		event = "VeryLazy",
+
+		config = function()
+			local dap = require("dap")
+			dap.adapters.codelldb = {
+				type = "server",
+				host = "127.0.0.1",
+				port = 13000, -- 💀 Use the port printed out or specified with `--port`
+			}
+
+			-- C C++ Rust
+			dap.adapters.codelldb = {
+				type = "server",
+				port = "${port}",
+				executable = {
+					command = "/home/maxi/.local/share/nvim/mason/packages/codelldb/extension/adapter/codelldb",
+					args = { "--port", "${port}" },
+				},
+			}
+
+			dap.configurations.cpp = {
+				{
+					name = "Launch file",
+					type = "codelldb",
+					request = "launch",
+					program = function()
+						vim.fn.system("make TARGET=program")
+						return vim.fn.getcwd() .. "/program"
+					end,
+					cwd = "${workspaceFolder}",
+					stopOnEntry = false,
+				},
+			}
+			dap.configurations.c = dap.configurations.cpp
+			dap.configurations.rust = dap.configurations.cpp
+		end,
 	},
-	config = function()
-    require("dapui").setup()
-		require("dap-go").setup()
+	{
+		"rcarriga/nvim-dap-ui",
+		event = "VeryLazy",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"mfussenegger/nvim-dap",
+		},
 
-		local dap, dapui = require("dap"), require("dapui")
+		config = function()
+			local dap, dapui = require("dap"), require("dapui")
 
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
---		dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
---			dapui.close()
-		end
+			dapui.setup()
 
-    	end,
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+		end,
+	},
 }
---
+++ .config/nvim/lua/plugins/debugging.lua
@@ -0,0 +1,17 @@
+return {}
+	-- {
+	-- 	"mfussenegger/nvim-dap",
+	-- 	dependencies = {
+	-- 		"rcarriga/nvim-dap-ui",
+	-- 	},
+
+	-- 	config = function()
+	-- 		local dap = require("dap")
+
+
+      -- require("dapui").setup()
+
+			
+	-- 	end,
+	-- },
+-- }
