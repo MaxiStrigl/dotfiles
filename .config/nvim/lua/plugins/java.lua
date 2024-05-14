255630b68d5de65fe525394fe4ee448258f84460
Maxi <max.03.strigl@hotmail.com>
2024-05-14 19:54:18 +0200

    minor changes

--- .config/nvim/lua/plugins/java.lua
+++ .config/nvim/lua/plugins/java.lua
@@ -1,50 +1,61 @@
 return {
-	"nvim-java/nvim-java",
-	dependencies = {
-		"nvim-java/lua-async-await",
-		"nvim-java/nvim-java-core",
-		"nvim-java/nvim-java-test",
-		"nvim-java/nvim-java-dap",
-		"MunifTanjim/nui.nvim",
-		"neovim/nvim-lspconfig",
-		"mfussenegger/nvim-dap",
-		{
-			"williamboman/mason.nvim",
-			opts = {
-				registries = {
-					"github:nvim-java/mason-registry",
-					"github:mason-org/mason-registry",
-				},
-			},
-		},
-	},
-	config = function()
-		require("java").setup()
+  {
+    "mfussenegger/nvim-jdtls",
+    config = function()
+      local config = {
+        cmd = {'java' },
+        root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
+      }
+      require("jdtls").start_or_attach(config);
+    end,
+  },
+  {
+    "nvim-java/nvim-java",
+    dependencies = {
+      "nvim-java/lua-async-await",
+      "nvim-java/nvim-java-core",
+      "nvim-java/nvim-java-test",
+      "nvim-java/nvim-java-dap",
+      "MunifTanjim/nui.nvim",
+      "neovim/nvim-lspconfig",
+      "mfussenegger/nvim-dap",
+      {
+        "williamboman/mason.nvim",
+        opts = {
+          registries = {
+            "github:nvim-java/mason-registry",
+            "github:mason-org/mason-registry",
+          },
+        },
+      },
+    },
+    config = function()
+      require("java").setup()
 
-		local keybinds = require("user.keybinds").lsp_keybinds
+      local keybinds = require("user.keybinds").lsp_keybinds
 
-    local on_attach = function(_client, buffer_number)
-			keybinds(buffer_number)
-		end
+      local on_attach = function(_client, buffer_number)
+        keybinds(buffer_number)
+      end
 
+      require("lspconfig").jdtls.setup({
+        settings = {
+          java = {
+            format = {
+              enabled = false,
+            },
+          },
+        },
+        on_attach = on_attach,
+      })
 
-		require("lspconfig").jdtls.setup({
-			settings = {
-				java = {
-					format = {
-						enabled = false,
-					},
-				},
-			},
-      on_attach = on_attach,
-		})
-
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.google_java_format
-			},
-			autostart = true,
-		})
-	end,
+      local null_ls = require("null-ls")
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.google_java_format
+        },
+        autostart = true,
+      })
+    end,
+  },
 }
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,90 +1,91 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPost" },
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPost" },
 
-	dependencies = {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"hrsh7th/nvim-cmp",
-		"nvimtools/none-ls.nvim",
-	},
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "hrsh7th/nvim-cmp",
+    "nvimtools/none-ls.nvim",
+  },
 
-	config = function()
-		local null_ls = require("null-ls")
-		local keybinds = require("user.keybinds").lsp_keybinds
+  config = function()
+    local null_ls = require("null-ls")
+    local keybinds = require("user.keybinds").lsp_keybinds
 
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"lua_ls",
-				"clangd",
-				"tsserver",
-				"html",
-				"bashls",
-				"cssls",
-				"jsonls",
-				"marksman",
-				"rust_analyzer",
-				"volar",
-			},
-		})
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "lua_ls",
+        "clangd",
+        "tsserver",
+        "html",
+        "bashls",
+        "cssls",
+        "jsonls",
+        "marksman",
+        "rust_analyzer",
+        "volar",
+      },
+    })
 
-		local servers = {
-			bashls = {},
-			clangd = {},
-			lua_ls = {},
-			tsserver = {},
-			html = {},
-			cssls = {},
-			dartls = {},
-			jsonls = {},
-			marksman = {},
-			volar = {},
-			rust_analyzer = {
-				cmd = {
-					"rustup",
-					"run",
-					"stable",
-					"rust_analyzer",
-				},
-			},
-			ocamllsp = {},
-		}
+    local servers = {
+      bashls = {},
+      clangd = {},
+      lua_ls = {},
+      tsserver = {},
+      html = {},
+      cssls = {},
+      dartls = {},
+      jsonls = {},
+      jdtls = {},
+      marksman = {},
+      volar = {},
+      rust_analyzer = {
+        cmd = {
+          "rustup",
+          "run",
+          "stable",
+          "rust_analyzer",
+        },
+      },
+      ocamllsp = {},
+    }
 
-		--cmp provides aditional capabilities
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		local default_capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+    --cmp provides aditional capabilities
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    local default_capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-		local on_attach = function(_client, buffer_number)
-			keybinds(buffer_number)
-		end
+    local on_attach = function(_client, buffer_number)
+      keybinds(buffer_number)
+    end
 
-		for name, config in pairs(servers) do
-			require("lspconfig")[name].setup({
-				capabilities = default_capabilities,
-				on_attach = on_attach,
-				settings = config.cmd,
-			})
-		end
+    for name, config in pairs(servers) do
+      require("lspconfig")[name].setup({
+        capabilities = default_capabilities,
+        on_attach = on_attach,
+        settings = config.cmd,
+      })
+    end
 
-		local formatting = null_ls.builtins.formatting
-		local diagnostics = null_ls.builtins.diagnostics
-		local code_actions = null_ls.builtins.code_actions
+    local formatting = null_ls.builtins.formatting
+    local diagnostics = null_ls.builtins.diagnostics
+    local code_actions = null_ls.builtins.code_actions
 
-		null_ls.setup({
-			border = "rounded",
-			sources = {
-				--formatting
-				formatting.google_java_format,
-				formatting.prettier,
-				formatting.stylua,
-				formatting.ocamlformat,
-				formatting.clang_format,
-			},
-			autostart = true,
-		})
+    null_ls.setup({
+      border = "rounded",
+      sources = {
+        --formatting
+        formatting.google_java_format,
+        formatting.prettier,
+        formatting.stylua,
+        formatting.ocamlformat,
+        formatting.clang_format,
+      },
+      autostart = true,
+    })
 
-		--Borderd for LspInfo ui
-		require("lspconfig.ui.windows").default_options.border = "rounded"
-	end,
+    --Borderd for LspInfo ui
+    require("lspconfig.ui.windows").default_options.border = "rounded"
+  end,
 }
